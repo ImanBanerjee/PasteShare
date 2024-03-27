5bd5212687acf755518e6874cac31184f91e6dde
ImanBanerjee <imanbanerjee03@gmail.com>
2024-03-27 14:15:17 +0530

    Version 1.2

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ function createLink() {
   var link = window.location.href + "?content=" + encodedContent;
   document.getElementById("linkInput").value = link;
   document.getElementById("linkContainer").style.display = "block";
+  document.getElementById("copyLinkButton").style.display = "block";
+  document.getElementById("copyTextButton").style.display = "block";
+  document.getElementById("clearTextButton").style.display = "block";
+  document.getElementById("pasteText").setAttribute("readonly", true); // Prevent editing after creating the link
 }
 
 function copyLink() {
@@ -11,6 +15,23 @@ function copyLink() {
   linkInput.select();
   document.execCommand("copy");
   alert("Link copied to clipboard!");
+}
+
+function copyText() {
+  var pasteText = document.getElementById("pasteText");
+  pasteText.select();
+  document.execCommand("copy");
+  alert("Text copied to clipboard!");
+}
+
+function clearText() {
+  document.getElementById("pasteText").value = "";
+  document.getElementById("linkInput").value = "";
+  document.getElementById("linkContainer").style.display = "none";
+  document.getElementById("copyLinkButton").style.display = "none";
+  document.getElementById("copyTextButton").style.display = "none";
+  document.getElementById("clearTextButton").style.display = "none";
+  document.getElementById("pasteText").removeAttribute("readonly");
 }
 
 // Check if there's content in the URL and populate textarea if found
